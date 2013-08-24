027214ccd4114832eb94c0edf6d38466419f227c
Thomas Sullivan <tom@nsp-precise-prod.vm.nsp.nectar.org.au>
2013-08-24 18:27:52 +1000

    Added script to convert JKS keystore to Apache ready key/certificate

---
+++ jks-to-crt-key.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+SRCKEYSTORE=${1}
+SRCALIAS=${2}
+
+echo -n 'Enter input keystore key (same for keystore and key): '
+read SRCKEYSTOREPASS
+
+DSTKEYSTORE=$(mktemp)
+DSTKEYSTOREPASS=$(dd if=/dev/urandom bs=1 count=32 2>/dev/null | md5sum | awk '{print $1}')
+
+rm ${DSTKEYSTORE}
+
+keytool -importkeystore \
+	-srckeystore ${SRCKEYSTORE} \
+	-srcstoretype JKS \
+	-srcstorepass ${SRCKEYSTOREPASS} \
+	-srcalias ${SRCALIAS} \
+	-destkeystore ${DSTKEYSTORE} \
+	-deststoretype PKCS12 \
+	-deststorepass ${DSTKEYSTOREPASS} \
+	-destkeypass ${DSTKEYSTOREPASS} 
+
+openssl pkcs12 -in ${DSTKEYSTORE} \
+	-nocerts \
+	-nodes \
+	-passin pass:${DSTKEYSTOREPASS} \
+	-out ${SRCALIAS}-private.key
+openssl pkcs12 -in ${DSTKEYSTORE} \
+	-clcerts \
+	-nokeys \
+	-nodes \
+	-passin pass:${DSTKEYSTOREPASS} \
+	-out ${SRCALIAS}-public.crt
+
+rm ${DSTKEYSTORE}
+
+echo "Public certificate at: ${SRCALIAS}-public.crt"
+echo "Private key at: ${SRCALIAS}-private.key"
