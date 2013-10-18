2002f015166aebfab737ae1f7636af4961a028b5
Tom Sullivan <tsullivan@unimelb.edu.au>
2013-10-18 12:33:42 +1100

    Add mysql-root script

---
+++ mysql-root.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+MYSQL_USER=debian-sys-maint
+MYSQL_PASSWORD=$(grep password /etc/mysql/debian.cnf | sort | uniq | awk '{print $3}')
+
+mysql --user=${MYSQL_USER} --password=${MYSQL_PASSWORD}
