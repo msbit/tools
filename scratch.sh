2b69f1421a8dd6111573bb1ec13263cd31919ddf
Tom Sullivan <tom@msbit.com.au>
2014-04-11 17:03:16 +1000

    Added script to operate in temp scratch

---
+++ scratch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+WORKING_DIR=$(mktemp -d -t scratch)
+
+pushd ${WORKING_DIR}
+PS1="\h:\W \u [$(basename ${WORKING_DIR})]\$ " HOME=${WORKING_DIR} bash --norc
+popd 
+rm -rf ${WORKING_DIR}
