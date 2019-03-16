1f6a8a608928af8e4d1ccdee391549c3caa91a1e
Tom Sullivan <tom@msbit.com.au>
2019-03-16 17:13:04 +1100

    Consolidate curl options

--- fetch-apple-event.sh
+++ fetch-apple-event.sh
@@ -30,12 +30,14 @@ CHILD_FILE=$(mktemp -t $$.XXXXXXXXXX)
 CONCAT_FILE=$(mktemp -t $$.XXXXXXXXXX)
 CHILD_DIR=$(mktemp -d -t $$.XXXXXXXXXX)
 
-curl -s -o "${MASTER_FILE}" "${MASTER_URL}"
+CURL_OPTS="--retry 4 -s -S"
+
+curl ${CURL_OPTS} -o "${MASTER_FILE}" "${MASTER_URL}"
 
 CHILD_URL=$(grep "${QUALITY}_vod.m3u8" "${MASTER_FILE}")
 VOD_PART_BASE="${CHILD_URL%%${QUALITY}_vod.m3u8}"
 
-curl -s -o "${CHILD_FILE}" "${CHILD_URL}"
+curl ${CURL_OPTS} -o "${CHILD_FILE}" "${CHILD_URL}"
 
 pushd "${CHILD_DIR}"
 
@@ -43,7 +45,7 @@ pushd "${CHILD_DIR}"
 grep "^${QUALITY}_vod_.*ts$" "${CHILD_FILE}" | while read -r VOD_PART_FILE
 do
   echo "file '${CHILD_DIR}/${VOD_PART_FILE}'" >> "${CONCAT_FILE}"
-  curl -s -O "${VOD_PART_BASE}${VOD_PART_FILE}"
+  curl ${CURL_OPTS} -O --retry 4 "${VOD_PART_BASE}${VOD_PART_FILE}"
   echo -n '.'
 done
 echo
