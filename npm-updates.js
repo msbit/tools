40188564ca5138d1966bff7c86c0032e1f32c32e
Tom Sullivan <tom@msbit.com.au>
2020-09-09 21:00:11 +1000

    Split off common NPM utilities

--- npm-updates.js
+++ npm-updates.js
@@ -1,122 +1,47 @@
 #!/usr/bin/env node
 
-const { spawnSync: spawn } = require('child_process');
-const {
-  appendFileSync: append,
-  readFileSync: read,
-  unlinkSync: unlink
-} = require('fs');
+const { spawnSync } = require('child_process');
+const { appendFileSync, unlinkSync } = require('fs');
 
-const diff = (before, after) => {
-  const output = {
-    added: {},
-    modified: {},
-    removed: {},
-    total: {}
-  };
+const { determineDependencyChanges, exists, mktemp, writeDependencySet } = require('./npm-common.js');
 
-  for (const [key, value] of Object.entries(before)) {
-    if (!(key in after)) {
-      output.removed[key] = value;
-      output.total[key] = value;
-      continue;
-    }
-
-    if (key in after && value !== after[key]) {
-      output.modified[key] = after[key];
-      output.total[key] = after[key];
-    }
-
-    delete after[key];
-  }
-
-  for (const [key, value] of Object.entries(after)) {
-    output.added[key] = value;
-    output.total[key] = value;
-  }
-
-  return output;
+const action = () => {
+  spawnSync('rm', ['-rf', 'node_modules', 'package-lock.json'], {
+    stdio: 'inherit'
+  });
+  spawnSync('npm', ['install'], {
+    stdio: 'inherit'
+  });
 };
 
-const exists = (obj) => Object.entries(obj).length > 0;
-
-const getDependencies = () => {
-  const { dependencies: deps, devDependencies: devDeps } = JSON.parse(read('package.json'));
-  const { dependencies: lockDeps } = JSON.parse(read('package-lock.json'));
-
-  const direct = Object.keys({ ...deps, ...devDeps }).sort();
-
-  const output = {
-    direct: {},
-    transitive: {}
-  };
-
-  direct.forEach((d) => { output.direct[d] = lockDeps[d].version; });
-
-  Object.keys(lockDeps)
-    .filter(d => !direct.includes(d))
-    .forEach((d) => { output.transitive[d] = lockDeps[d].version; });
-
-  return output;
-};
-
-const mktemp = () => {
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let tmpFile = 'tmp.';
-  for (let i = 0; i < 8; i++) {
-    const index = Math.floor(Math.random() * chars.length);
-    tmpFile += chars[index];
-  }
-  return `${process.env.TMPDIR}${tmpFile}`;
-};
-
-const writeDependencySet = (file, { added, removed, modified }) => {
-  writeDependencies(file, 'Added', added);
-  writeDependencies(file, 'Removed', removed);
-  writeDependencies(file, 'Modified', modified);
-};
-
-const writeDependencies = (file, message, dependencies, prefix = ' ') => {
-  if (!exists(dependencies)) { return; }
-
-  append(file, `${prefix}${message}\n\n`);
-  for (const [key, value] of Object.entries(dependencies)) {
-    append(file, `${prefix} * ${key}: ${value}\n`);
-  }
-  append(file, '\n');
-};
-
-const before = getDependencies();
-
-spawn('rm', ['-rf', 'node_modules', 'package-lock.json'], { stdio: 'inherit' });
-
-spawn('npm', ['install'], { stdio: 'inherit' });
-
-const after = getDependencies();
-
-const direct = diff(before.direct, after.direct);
-const transitive = diff(before.transitive, after.transitive);
-
-if (exists(direct.total) || exists(transitive.total)) {
-  spawn('git', ['checkout', '-b', `npm-updates-${Date.now()}`], { stdio: 'inherit' });
-  spawn('git', ['add', 'package-lock.json'], { stdio: 'inherit' });
+const reaction = (direct, transitive) => {
+  spawnSync('git', ['checkout', '-b', `npm-updates-${Date.now()}`], {
+    stdio: 'inherit'
+  });
+  spawnSync('git', ['add', 'package-lock.json'], {
+    stdio: 'inherit'
+  });
 
   const file = mktemp();
 
-  append(file, 'Dependency updates\n\n');
+  appendFileSync(file, 'Dependency updates\n\n');
 
   if (exists(direct.total)) {
-    append(file, 'Direct dependencies\n\n');
+    appendFileSync(file, 'Direct dependencies\n\n');
 
     writeDependencySet(file, direct);
   }
 
   if (exists(transitive.total)) {
-    append(file, 'Transitive dependencies\n\n');
+    appendFileSync(file, 'Transitive dependencies\n\n');
 
     writeDependencySet(file, transitive);
   }
 
-  spawn('git', ['commit', '--file', file], { stdio: 'inherit' });
-  unlink(file);
-}
+  spawnSync('git', ['commit', '--file', file], {
+    stdio: 'inherit'
+  });
+  unlinkSync(file);
+};
+
+determineDependencyChanges(action, reaction);
